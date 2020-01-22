12cd3de7ce5be8964c015efa416525161c61a179
xxxx <18655770810@163.com>
2020-01-22 17:15:39 +0800

    订单创建

--- src/test/java/com/imooc/mall/service/Impl/OrderServiceImplTest.java
+++ src/test/java/com/imooc/mall/service/Impl/OrderServiceImplTest.java
@@ -1,23 +1,29 @@
 package com.imooc.mall.service.Impl;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.imooc.mall.MallApplicationTests;
+import com.imooc.mall.enums.ResponseEnum;
 import com.imooc.mall.responseVo.OrderVo;
 import com.imooc.mall.responseVo.ResponseVo;
 import com.imooc.mall.service.IOrderService;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.Assert;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-
-import static org.junit.Assert.*;
 
 @Slf4j
 public class OrderServiceImplTest extends MallApplicationTests {
     @Autowired
     private IOrderService orderService;
 
+    private Gson gson = new GsonBuilder().setPrettyPrinting().create();
+
     @Test
     public void create() {
         ResponseVo<OrderVo> responseVo = orderService.create(5, 5);
-        log.info("create = {}", responseVo);
+        log.info("create = {}", gson.toJson(responseVo));
+        Assert.assertEquals(ResponseEnum.SUCCESS.getCode(), responseVo.getStatus());
+
     }
 }
